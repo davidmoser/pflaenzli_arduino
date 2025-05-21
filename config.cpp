7c64ce4533af2981759feea26fdbed48c2af776a
David Moser <david.moser@gmx.net>
2025-05-21 12:24:57 +0200

    change default settings

--- config.cpp
+++ config.cpp
@@ -2,9 +2,9 @@
 
 // Operational Parameters
 const int configuration_id = 1;          // id of configuration record on server
-volatile int moistureThreshold = 5;
+volatile int moistureThreshold = 0;
 volatile int measurementInterval = 600;  // in seconds
-volatile int pumpDuration = 120;         // in seconds
+volatile int pumpDuration = 5;         // in seconds
 volatile int valveDuration = 7;          // in seconds
 volatile int consecutivePumps = 0;       // number of times pump has run after consecutive measurements
 volatile int maxConsecutivePumps = 5;    // number of times pump may run consecutively, otherwise it's disabled
